fa27e1e3e327344c1ef02d1dd89b3d96ed157065
jagannathkvjkv@gmail.com <you@example.com>
2020-05-03 11:48:28 +0530

    changes to main js

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
-const msg_typed = document.querySelector(".type-area");
-const chat_area = document.querySelector(".chat-area");
+const socket = io();
+const msg = document.getElementById('typedMsg');
 
 
-function send() {
-    console.log(msg_typed.value);
+
+const send = document.getElementById('send');
+
+const username = prompt('Enter Your Name');
 
 
-chat_area.value = msg_typed.value;
 
+socket.emit('user', username);
+
+//message from server
+socket.on('msg', data =>{
+    
+
+    outputMessage(data);
+
+});
+
+send.addEventListener('click', (e) => {
+    e.preventDefault();
+   
+   socket.emit('chatMessage', msg.value);
+
+   msg.value='';
+
+   document.getElementById("typedMsg").focus();
+  
+    
+});
+ 
+//output message
+function outputMessage(data){
+
+    const div = document.createElement('div');
+    div.classList.add('chatmsg','shadow','rounded');
+    div.innerHTML = `${data}`;
+    document.getElementById('messages').appendChild(div);
+    setInterval(updateScroll(),1000);
+
+}
+
+//scroll
+function updateScroll(){
+    var element = document.getElementById("messages");
+    element.scrollTop = element.scrollHeight;
 }
